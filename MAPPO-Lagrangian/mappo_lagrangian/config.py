bcd83ea76321766cf0ecc7de764fdedcaac7b8ac
Chunran <2684487313@qq.com>
2025-10-05 13:06:54 +0800

    修改命令参数

--- MAPPO-Lagrangian/mappo_lagrangian/config.py
+++ MAPPO-Lagrangian/mappo_lagrangian/config.py
@@ -156,7 +156,7 @@ def get_config():
 
     # prepare parameters
     parser.add_argument("--algorithm_name", type=str,
-                        default=' ', choices=[ "mappo_lagr"])
+                        default=' ', choices=[ "mappo_lagr","async_mappo_lagr"])
 
     parser.add_argument("--experiment_name", type=str, default="check", help="an identifier to distinguish different experiment.")
     parser.add_argument("--seed", type=int, default=1, help="Random seed for numpy/torch")
